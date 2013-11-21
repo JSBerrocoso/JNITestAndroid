df90ff4e20cc66b254053489bae36cff8f7ec5fb
jsberrocoso <jsbs87@gmail.com>
2013-11-21 01:04:06 +0100

    first commit

---
+++ obj/local/armeabi/objs/ndksample/ndksample.o.d
@@ -0,0 +1,20 @@
+C:/Users/Djimn/workspace/JNITestAndroid//obj/local/armeabi/objs/ndksample/ndksample.o: \
+ C:/Users/Djimn/workspace/JNITestAndroid//jni/ndksample.c \
+ C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/string.h \
+ C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/malloc.h \
+ C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/jni.h
+
+C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/string.h:
+
+C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+C:/android/android-ndk-r9b/platforms/android-3/arch-arm/usr/include/jni.h:
